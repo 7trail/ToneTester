21e06739e8f130acf7814e4b8fddde51ff8717f5
Paradise072 <paradise072@icloud.com>
2025-04-29 17:23:26 -0500

    Updated Everything

--- ToneTester/Main.Designer.cs
+++ ToneTester/Main.Designer.cs
@@ -29,157 +29,148 @@ namespace ToneTester
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new System.Windows.Forms.Button();
-            textBox1 = new System.Windows.Forms.TextBox();
-            label1 = new System.Windows.Forms.Label();
-            checkBox1 = new System.Windows.Forms.CheckBox();
-            richTextBox1 = new System.Windows.Forms.RichTextBox();
-            pictureBox2 = new System.Windows.Forms.PictureBox();
-            label2 = new System.Windows.Forms.Label();
-            menuStrip1 = new System.Windows.Forms.MenuStrip();
-            menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
-            menuStrip1.SuspendLayout();
-            SuspendLayout();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
             // 
             // button1
             // 
-            button1.Font = new System.Drawing.Font("Segoe UI", 25F);
-            button1.Location = new System.Drawing.Point(12, 328);
-            button1.Name = "button1";
-            button1.Size = new System.Drawing.Size(200, 56);
-            button1.TabIndex = 0;
-            button1.Text = "Evaluate";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(12, 328);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Evaluate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.UpdateUI);
             // 
             // textBox1
             // 
-            textBox1.Location = new System.Drawing.Point(12, 27);
-            textBox1.Multiline = true;
-            textBox1.Name = "textBox1";
-            textBox1.Size = new System.Drawing.Size(200, 295);
-            textBox1.TabIndex = 1;
+            this.textBox1.Location = new System.Drawing.Point(12, 27);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 295);
+            this.textBox1.TabIndex = 1;
             // 
             // label1
             // 
-            label1.AutoSize = true;
-            label1.Font = new System.Drawing.Font("Segoe UI", 20F);
-            label1.Location = new System.Drawing.Point(218, 24);
-            label1.Name = "label1";
-            label1.Size = new System.Drawing.Size(117, 37);
-            label1.TabIndex = 2;
-            label1.Text = "Emotion";
-            // 
-            // checkBox1
-            // 
-            checkBox1.AutoSize = true;
-            checkBox1.Location = new System.Drawing.Point(218, 175);
-            checkBox1.Name = "checkBox1";
-            checkBox1.Size = new System.Drawing.Size(94, 19);
-            checkBox1.TabIndex = 3;
-            checkBox1.Text = "Performance";
-            checkBox1.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(218, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 37);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Emotion";
             // 
             // richTextBox1
             // 
-            richTextBox1.Location = new System.Drawing.Point(218, 56);
-            richTextBox1.Name = "richTextBox1";
-            richTextBox1.ReadOnly = true;
-            richTextBox1.Size = new System.Drawing.Size(340, 96);
-            richTextBox1.TabIndex = 9;
-            richTextBox1.Text = "";
+            this.richTextBox1.Location = new System.Drawing.Point(218, 56);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(340, 96);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
             // 
             // pictureBox2
             // 
-            pictureBox2.BackColor = System.Drawing.SystemColors.ControlLight;
-            pictureBox2.Location = new System.Drawing.Point(330, 175);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new System.Drawing.Size(228, 214);
-            pictureBox2.TabIndex = 13;
-            pictureBox2.TabStop = false;
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pictureBox2.Location = new System.Drawing.Point(330, 175);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(228, 214);
+            this.pictureBox2.TabIndex = 13;
+            this.pictureBox2.TabStop = false;
             // 
             // label2
             // 
-            label2.AutoSize = true;
-            label2.Location = new System.Drawing.Point(12, 387);
-            label2.Name = "label2";
-            label2.Size = new System.Drawing.Size(69, 15);
-            label2.TabIndex = 17;
-            label2.Text = "Update Text";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 387);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 15);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Update Text";
             // 
             // menuStrip1
             // 
-            menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { menuToolStripMenuItem, helpToolStripMenuItem, toolsToolStripMenuItem });
-            menuStrip1.Location = new System.Drawing.Point(0, 0);
-            menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new System.Drawing.Size(570, 24);
-            menuStrip1.TabIndex = 18;
-            menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem,
+            this.helpToolStripMenuItem,
+            this.toolsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(570, 24);
+            this.menuStrip1.TabIndex = 18;
+            this.menuStrip1.Text = "menuStrip1";
             // 
             // menuToolStripMenuItem
             // 
-            menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] { quitToolStripMenuItem });
-            menuToolStripMenuItem.Name = "menuToolStripMenuItem";
-            menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
-            menuToolStripMenuItem.Text = "&Menu";
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quitToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "&Menu";
             // 
             // quitToolStripMenuItem
             // 
-            quitToolStripMenuItem.Name = "quitToolStripMenuItem";
-            quitToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4;
-            quitToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
-            quitToolStripMenuItem.Text = "Quit";
-            quitToolStripMenuItem.Click += quitToolStripMenuItem_Click;
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.quitToolStripMenuItem.Text = "Quit";
             // 
             // helpToolStripMenuItem
             // 
-            helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] { aboutToolStripMenuItem });
-            helpToolStripMenuItem.Name = "helpToolStripMenuItem";
-            helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
-            helpToolStripMenuItem.Text = "&Help";
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "&Help";
             // 
             // aboutToolStripMenuItem
             // 
-            aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-            aboutToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F1;
-            aboutToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
-            aboutToolStripMenuItem.Text = "About";
-            aboutToolStripMenuItem.ToolTipText = "Shows information about the program.";
-            aboutToolStripMenuItem.Click += aboutToolStripMenuItem_Click;
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F1;
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.ToolTipText = "Shows information about the program.";
             // 
             // toolsToolStripMenuItem
             // 
-            toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
-            toolsToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
-            toolsToolStripMenuItem.Text = "&Tools";
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.toolsToolStripMenuItem.Text = "&Tools";
             // 
-            // Form1
+            // Main
             // 
-            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            ClientSize = new System.Drawing.Size(570, 411);
-            Controls.Add(label2);
-            Controls.Add(pictureBox2);
-            Controls.Add(richTextBox1);
-            Controls.Add(checkBox1);
-            Controls.Add(label1);
-            Controls.Add(textBox1);
-            Controls.Add(button1);
-            Controls.Add(menuStrip1);
-            MainMenuStrip = menuStrip1;
-            Name = "Form1";
-            Text = "ToneTester";
-            Load += Form1_Load;
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
-            menuStrip1.ResumeLayout(false);
-            menuStrip1.PerformLayout();
-            ResumeLayout(false);
-            PerformLayout();
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 411);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Main";
+            this.Text = "ToneTester";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
@@ -187,7 +178,6 @@ namespace ToneTester
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Label label2;
--- ToneTester/Main.cs
+++ ToneTester/Main.cs
@@ -14,116 +14,47 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ToneTester_AI;
 
 namespace ToneTester
 {
     public partial class Main : Form
     {
-        static Dictionary<string,List<string>> dict;
-        static Dictionary<string, List<string>> dict2;
 
         static Random rnd = new Random();
+        public Dictionary<string, Color> moodColors = new Dictionary<string, Color>() { ["joy"] = Color.Green, ["fear"] = Color.BlueViolet, ["surprise"] = Color.Orange, ["anger"] = Color.Red, ["sadness"] = Color.Blue, ["love"] = Color.Pink };
+        public static bool evaluated = false;
+        public static Dictionary<string, float> moods = new Dictionary<string, float>();
+
         public Main()
         {
             InitializeComponent();
             CenterToScreen(); // Just makes the UI appear in the center of the screen. 
-
-            string s = Properties.Resources.NewResource;
-            string[] lines = s.Split(
-                new string[] { Environment.NewLine },
-                StringSplitOptions.None
-            );
-
-            var tempDict = new Dictionary<string, List<string>>();
-            foreach (string str in lines)
-            {
-                string[] splits = str.Split(',',2);
-
-                KeyValuePair<string, string> pair = new KeyValuePair<string, string>(splits[0],splits[1]);
-
-                string[] split = pair.Value.Split(new Char[] { ';', '|', ',' },
-                                 StringSplitOptions.RemoveEmptyEntries);
-                if (!tempDict.ContainsKey(pair.Key))
-                {
-                    tempDict.Add(pair.Key, split.ToList());
-                } else
-                {
-                    foreach(string ss in split)
-                    {
-                        if (ss != "" && !(ss.Distinct().Count()==1&&ss.ElementAt(0)!=','))
-                        {
-                            tempDict[pair.Key].Add(ss);
-                        }
-                    }
-                }
-            }
-            dict = tempDict;
-
-            s = Properties.Resources.String1;
-            lines = s.Split(
-                new string[] { Environment.NewLine },
-                StringSplitOptions.None
-            );
-            //dict = lines.Select(line => line.Split(',')).ToDictionary(line => line[0], line => line[1]);
-            var tempDict2 = new Dictionary<string, List<string>>();
-            foreach (string str in lines)
-            {
-                string[] splits = str.Split(',');
-
-                KeyValuePair<string, string> pair = new KeyValuePair<string, string>(splits[0], splits[1]);
-
-                string[] split = pair.Value.Split(new Char[] { ';', '|', ',' },
-                                 StringSplitOptions.RemoveEmptyEntries);
-                if (!tempDict2.ContainsKey(pair.Key))
-                {
-                    tempDict2.Add(pair.Key, split.ToList());
-                }
-                else
-                {
-                    foreach (string ss in split)
-                    {
-                        if (ss != "" && !(ss.Distinct().Count() == 1 && ss.ElementAt(0) != ','))
-                        {
-                            tempDict2[pair.Key].Add(ss);
-                            //Debug.Write("Added " + ss);
-                        }
-                    }
-                }
-            }
-            dict2 = tempDict2;
+            pictureBox2.Paint += pictureBox2_Paint;
+            pictureBox2.Resize += pictureBox2_Resize;
         }
-        public static bool evaluated = false;
-        public static Dictionary<string, float> moods = new Dictionary<string, float>();
 
-        private void pictureBox2_Resize(object sender,
-        EventArgs pe)
+        private void pictureBox2_Resize(object sender, EventArgs pe) // If the screen changes size we have to redraw the pie chart
         {
             pictureBox2.Invalidate();
         }
             
-        private void pictureBox2_Paint(object sender,
-        System.Windows.Forms.PaintEventArgs pe)
+        private void pictureBox2_Paint(object sender, System.Windows.Forms.PaintEventArgs pe)
         {
-
-            // Declares the Graphics object and sets it to the Graphics object  
-            // supplied in the PaintEventArgs.  
             Graphics g = pe.Graphics;
             g.Clear(pictureBox2.BackColor);
             g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
             Pen p = new Pen(Color.Black, 2);
-            // Insert code to paint the form here.  
+
             Rectangle rec = new Rectangle(0, 0, pictureBox2.Width-2, pictureBox2.Height-2);
 
             if (evaluated)
             {
-                
                 float total = 0;
                 foreach (KeyValuePair<string, float> pair in moods)
                 {
-                    //Debug.Write("Showing pair");
-                    float degree = pair.Value * 360;
+                    float degree = pair.Value * 360; // To get the size of the pie slice
 
-                    //g.DrawPie(p, rec, total, degree);
                     Brush b1 = new SolidBrush(moodColors[pair.Key.ToLower()]);
                     g.FillPie(b1, rec, total, degree);
                     total += degree;
@@ -131,64 +62,10 @@ namespace ToneTester
                 }
             }
             p.Dispose();
-            
         }
 
 
-        public struct SentenceData
-        {
-            public string type;
-            public float confidence;
-            public float[] score;
-        }
-        public SentenceData GetMostProminent(string input, bool loadPieValues = false)
-        {
-            string str = "";
-            SentenceData data = new SentenceData();
-            dynamic sampleData = null;
-            dynamic result = null;
-            Dataset.ModelOutput output;
-            if (checkBox1.Checked)
-            {
-                sampleData = new DatasetLightweight.ModelInput()
-                {
-                    Col0 = input,
-                };
-                result = DatasetLightweight.Predict(sampleData);
-            }
-            else
-            {
-                sampleData = new Dataset.ModelInput()
-                {
-                    Col0 = input,
-                };
-                result = Dataset.Predict(sampleData);
-            }
-
-            if (result != null)
-            {
-                data.type = result.PredictedLabel;
-                data.confidence = result.Score[1];
-                data.score = result.Score;
-            }
-
-            if (loadPieValues && result != null)
-            {
-                evaluated = true;
-                int i = 0;
-                foreach(float val in result.Score)
-                {
-                    
-                    moods[moodColors.Keys.ElementAt(i)] = val;
-                    //Debug.WriteLine("Added mood");
-                    i++;
-                }
-                pictureBox2.Invalidate();
-                //foreach(string type in GetSlotNames())
-            }
-
-            return data;
-        }
+        
         private void Form1_Load(object sender, EventArgs e)
         {
             pictureBox2.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox2_Paint);
@@ -196,13 +73,20 @@ namespace ToneTester
             this.DoubleBuffered = true;
         }
 
-        
-
-        private void button1_Click(object sender, EventArgs e)
+        private void UpdateUI(object sender, EventArgs e)
         {
-            label1.Text = "Mood: " + GetMostProminent(textBox1.Text.ToLower(),true).type.FirstCharToUpper();
+            SentenceData result = Interpreter.GetMostProminent(textBox1.Text.ToLower(), true);
+            evaluated = true;
+            int i = 0;
+            foreach (float val in result.score)
+            {
+                moods[moodColors.Keys.ElementAt(i)] = val;
+                i++;
+            }
+            label1.Text = "Mood: " + result.type.FirstCharToUpper();
             richTextBox1.Text = textBox1.Text.ToLower().CapitalizeFirst();
             SetBoxText(richTextBox1);
+            pictureBox2.Invalidate(); // To force the pie chart to redraw and display
         }
 
         public void SetBoxText(RichTextBox box)
@@ -210,7 +94,7 @@ namespace ToneTester
             int index = 0;
             foreach (string word in box.Text.Split(' '))
             {
-                string mood = GetMostProminent(word).type;
+                string mood = Interpreter.GetMostProminent(word).type;
                 int selectStart = box.SelectionStart;
                 if (moodColors.ContainsKey(mood.ToLower()))
                 {
@@ -222,11 +106,6 @@ namespace ToneTester
                 index += word.Length + 1;
             }
         }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-        }
-        public Dictionary<string, Color> moodColors = new Dictionary<string, Color>() { ["joy"] = Color.Green,  ["fear"] = Color.BlueViolet, ["surprise"] = Color.Orange, ["anger"] = Color.Red, ["sadness"] = Color.Blue, ["love"] = Color.Pink };
 
         // Quit function
         private void quitToolStripMenuItem_Click(object sender, EventArgs e)
@@ -240,36 +119,5 @@ namespace ToneTester
         {
             MessageBox.Show("ToneTester 0.2\nBuilt by Austin Phillips\n2022-2025");
         }
-    }
-}
-public static class StringExtensions
-{
-    public static string FirstCharToUpper(this string input) =>
-        input switch
-        {
-            "" => throw new ArgumentException($"{nameof(input)} cannot be empty", nameof(input)),
-            _ => string.Concat(input[0].ToString().ToUpper(), input.AsSpan(1))
-        };
-    public static string CapitalizeFirst(this string s)
-    {
-        bool IsNewSentense = true;
-        var result = new StringBuilder(s.Length);
-        for (int i = 0; i < s.Length; i++)
-        {
-            if (IsNewSentense && char.IsLetter(s[i]))
-            {
-                result.Append(char.ToUpper(s[i]));
-                IsNewSentense = false;
-            }
-            else
-                result.Append(s[i]);
-
-            if (s[i] == '!' || s[i] == '?' || s[i] == '.')
-            {
-                IsNewSentense = true;
-            }
-        }
-
-        return result.ToString();
     }
 }
